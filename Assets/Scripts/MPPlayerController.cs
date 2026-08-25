9be4fe1781543a631a0662bddce983f583fa7046
yaakgold <yaakovgoldberg007@gmail.com>
2026-08-24 23:31:22 -0700

    NETWORKING WORKS!!!!!!!!!!!!

---
+++ Assets/Scripts/MPPlayerController.cs
@@ -0,0 +1,7 @@
+using Unity.Netcode;
+using UnityEngine;
+
+public class MpPlayerController : NetworkBehaviour
+{
+    [SerializeField] private string playerName;
+}
--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,39 +1,137 @@
+using System;
+using Blocks.Sessions.Common;
 using Unity.Netcode;
+using Unity.Properties;
+using Unity.Services.Authentication;
+using Unity.Services.Lobbies;
+using Unity.Services.Multiplayer;
 using UnityEngine;
-using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
-using UnityEngine.UI;
+using UnityEngine.UIElements;
+using WebSocketSharp;
 
 public class MainMenu : MonoBehaviour
 {
-    [SerializeField] private Button startHostButton;
-    [SerializeField] private Button startClientButton;
     [SerializeField] private NetworkObject projectManagerPrefab;
+    [SerializeField] private PanelRenderer panelRenderer;
+    [SerializeField] private SessionSettings sessionSettings;
+    
+    private VisualElement _root;
+    private SessionObserver _sessionObserver;
+    private ISession _session;
 
-    // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        startHostButton.onClick.AddListener(StartHost);
-        startClientButton.onClick.AddListener(StartClient);
+        _sessionObserver = new SessionObserver(sessionSettings.sessionType);
+        
+        _sessionObserver.AddingSessionStarted += SessionObserverOnAddingSessionStarted; 
+        _sessionObserver.AddingSessionFailed += SessionObserverOnAddingSessionFailed;
+        _sessionObserver.SessionAdded += SessionObserverOnSessionAdded;
+    }
+    
+    void OnEnable()
+    {
+        panelRenderer.RegisterUIReloadCallback(OnUIReload);
     }
 
-    void StartClient()
+    void OnDisable()
     {
-        NetworkManager.Singleton.StartClient();
-        DeactivateButtons();
+        panelRenderer.UnregisterUIReloadCallback(OnUIReload);
+    }
+    
+    private void SessionObserverOnSessionAdded(ISession obj)
+    {
+        _session = obj;
+
+        var playerName = _root.Q<TextField>("playerName")?.value;
+        if (!playerName.IsNullOrEmpty())
+            AuthenticationService.Instance.UpdatePlayerNameAsync(playerName);
+        
+        _session.RemovedFromSession += SessionOnRemovedFromSession;
+        _session.PlayerJoined += SessionOnPlayerJoined;
+        _session.PlayerLeaving += SessionOnPlayerLeaving;
+
+        if (_session.IsHost)
+        {
+            _root.Q("btnStart").style.display = DisplayStyle.Flex;
+        }
     }
 
-    void StartHost()
+    private void SessionOnPlayerLeaving(string obj)
     {
-        NetworkManager.Singleton.StartHost();
+        print(_session.PlayerCount);
+        if (_session.PlayerCount <= 2)
+        {
+            _root.Q("btnStart").enabledSelf = false;
+        }
+    }
+
+    private void SessionOnRemovedFromSession()
+    {
+        _root.Q("join").style.display = DisplayStyle.Flex;
+        _root.Q("currentSession").style.display = DisplayStyle.None;
+    }
+
+    private void SessionObserverOnAddingSessionStarted(AddingSessionOptions obj)
+    {
+        _root.Q("join").style.display = DisplayStyle.None;
+        _root.Q("currentSession").style.display = DisplayStyle.Flex;
+    }
+
+    private void SessionObserverOnAddingSessionFailed(AddingSessionOptions arg1, SessionException arg2)
+    {
+        _root.Q("join").style.display = DisplayStyle.Flex;
+        _root.Q("currentSession").style.display = DisplayStyle.None;
+    }
+
+    private void SessionOnPlayerJoined(string obj)
+    {
+        print(_session.PlayerCount);
+        if (_session.PlayerCount >= 2)
+        {
+            _root.Q("btnStart").enabledSelf = true;
+        }
+    }
+
+    void OnUIReload(PanelRenderer renderer, VisualElement rootElement)
+    {
+        _root = rootElement;
+        
+        _root.Q<Button>("btnJoin").RegisterCallback<ClickEvent>(OnJoin);
+        _root.Q<Button>("btnExit").RegisterCallback<ClickEvent>(OnExit);
+        
+        _root.Q<Button>("btnStart").RegisterCallback<ClickEvent>(StartGame);
+        
+        _root.Q("quickJoin").RegisterCallback<ClickEvent>(OnDisplayChange);
+    }
+
+    private void OnDisplayChange(ClickEvent evt)
+    {
+        var obj = _root.Q("quickJoin");
+        
+        _root.Q("currentSession").style.display = obj.style.display== DisplayStyle.Flex ?  DisplayStyle.None : DisplayStyle.Flex;
+    }
+
+    private void OnExit(ClickEvent evt)
+    {
+        Application.Quit();
+    }
+
+    private void OnReturn(ClickEvent evt)
+    {
+        _root.Q("mainMenu").style.display = DisplayStyle.Flex;
+        _root.Q("join").style.display = DisplayStyle.None;
+    }
+
+    private void OnJoin(ClickEvent evt)
+    {
+        _root.Q("mainMenu").style.display = DisplayStyle.None;
+        _root.Q("join").style.display = DisplayStyle.Flex;
+    }
+    
+    void StartGame(ClickEvent evt)
+    {
         NetworkManager.Singleton.SceneManager.LoadScene("Main Game", LoadSceneMode.Single);
         NetworkManager.Singleton.SpawnManager.InstantiateAndSpawn(projectManagerPrefab);
-        DeactivateButtons();
-    }
-
-    void DeactivateButtons()
-    {
-        startHostButton.interactable = false;
-        startClientButton.interactable = false;
     }
 }
